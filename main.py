e71eb58dac44f11d4adcd395413732102be84735
sambakker4 <sambakkercow@gmail.com>
2024-12-08 13:49:58 -0500

    main game loop

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        screen.fill("black")
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
